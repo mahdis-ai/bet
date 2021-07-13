c93863cf6821edc3c6fad1441f64c0055440b493
mahdis_ai <mahdisabedi127@gmail.com>
2021-07-13 00:46:54 -0700

    final

--- bet.py
+++ bet.py
@@ -8,9 +8,8 @@ from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import re
 import time
-def login(user_name,pass_word,surname):
-    driver = webdriver.Chrome('C:/Users/user/pyprojects/lib/chromedriver.exe')
-    driver.get('https://1xbet-648374.top/registration/?tag=d_49778m_97c_101')
+def login(driver,user_name,pass_word,surname):
+    driver.get('https://1xbet-188659.top/registration/?tag=d_49778m_97c_101')
     time.sleep(5)
     block = driver.find_element(By.XPATH,"//a[@href='#deny']").click()
     login_form=driver.find_element_by_id('curLoginForm').click()
@@ -25,50 +24,37 @@ def login(user_name,pass_word,surname):
     login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='auth-button auth-button--block auth-button--slide-up-hover auth-button--theme-secondary']"))).click()
     time.sleep(3)
     #uncomment these lines if needed
-    #surname= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[id='input_surname']")))
-    #surname.send_keys("business")
-    #confirm = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='block-window__btn']"))).click()
+    try:
+        surname= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[id='input_surname']")))
+        surname.send_keys("business")
+        confirm = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='block-window__btn']"))).click()
+    except Exception:
+        pass
+   
     time.sleep(5)
     current_currency=driver.find_element(By.XPATH,"//div[@class='top-b__account']").click()
     other_currency=driver.find_elements_by_xpath("//div[@class='user-balance-list__item']")
     other_currency[1].click()
     print("logged in successfully")
-    return 0
 
 
 
 
 
-def bet_on_game(match_link,even_type,price,user_name,pass_word,surname):
-    driver = webdriver.Chrome('C:/Users/user/pyprojects/lib/chromedriver.exe')
-    driver.get('https://1xbet-648374.top/registration/?tag=d_49778m_97c_101')
-    time.sleep(5)
-    block = driver.find_element(By.XPATH,"//a[@href='#deny']").click()
-    login_form=driver.find_element_by_id('curLoginForm').click()
-    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[id='auth_id_email']")))
-    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[id='auth-form-password']")))
-    username.clear()
-#username_of_any_acount
-    username.send_keys(user_name)
-    password.clear()
-#password_of_that_acount
-    password.send_keys(pass_word)
-    login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='auth-button auth-button--block auth-button--slide-up-hover auth-button--theme-secondary']"))).click()
-    time.sleep(3)
-    #uncomment these lines if needed
-    #surname= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[id='input_surname']")))
-    #surname.send_keys("business")
-    #confirm = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='block-window__btn']"))).click()
-    time.sleep(5)
-    current_currency=driver.find_element(By.XPATH,"//div[@class='top-b__account']").click()
-    other_currency=driver.find_elements_by_xpath("//div[@class='user-balance-list__item']")
-    other_currency[1].click()
-    
-    match_link=driver.get(match_link)
+
+def bet_on_game(driver,match_link,even_type,price):
+    try:
+        match_link=driver.get(match_link)
+    except Exception:
+        print("can not find match")
+        return -1
     time.sleep(5)
 
     time.sleep(5)
-    quarter_number=driver.find_element(By.XPATH, "//li[@class='o-tablo-info-list__item']").text
+    try:
+        quarter_number=driver.find_element(By.XPATH, "//li[@class='o-tablo-info-list__item']").text
+    except Exception:
+        print("game is not underway")
     try:
         menu=driver.find_element(By.XPATH,"//div[@class='multiselect s-scoreboard-nav-multiselect']").click()
     except Exception:
@@ -102,7 +88,6 @@ def bet_on_game(match_link,even_type,price,user_name,pass_word,surname):
                 flag=True
                 i+=1
                 print("Error")
-                break
     
  
         else:
@@ -117,7 +102,6 @@ def bet_on_game(match_link,even_type,price,user_name,pass_word,surname):
                 flag=True
                 i+=1
                 print("Error")
-                break
         if(flag==False):
             break
         else:
@@ -127,15 +111,25 @@ def bet_on_game(match_link,even_type,price,user_name,pass_word,surname):
                 return -1
                 
    
+    if(flag==False):
+        bet_price= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[class='c-spinner__input bet_sum_input']")))
+        bet_price.send_keys(price)
+        time.sleep(5)
+        try:
+            place_bet=WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='c-btn c-btn--size-l c-btn--block c-btn--gradient c-btn--gradient-accent u-upcase']"))).click()
+        except Exception:
+            print("can not find ")
+        ok=WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='c-btn c-btn--size-m c-btn--block c-btn--gradient c-btn--gradient-accent u-upcase']"))).click()
+        print("betting has been done")
+        return 1
+    else:
+        print("failed")
+        return-1
+def betting():
+    driver = webdriver.Chrome('C:/Users/user/pyprojects/lib/chromedriver.exe')
+    login(driver,"300244749","MyDream0","business")
+    bet_on_game(driver,'https://ppbemizn.top/live/Basketball/24591-Argentina-LNB/313144030-Union-De-Santa-Fe-Estudiantes-Tucumn/',"yes",5000)
 
-
-    bet_price= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[class='c-spinner__input bet_sum_input']")))
-    bet_price.send_keys(price)
-    time.sleep(5)
-    place_bet=WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='c-btn c-btn--size-l c-btn--block c-btn--gradient c-btn--gradient-accent u-upcase']"))).click()
-    ok=WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='c-btn c-btn--size-m c-btn--block c-btn--gradient c-btn--gradient-accent u-upcase']"))).click()
-    print("betting has been done")
-    return 1
 
 def check_result(user_name,pass_word,surname):
     driver = webdriver.Chrome('C:/Users/user/pyprojects/lib/chromedriver.exe')
@@ -170,13 +164,13 @@ def check_result(user_name,pass_word,surname):
     time.sleep(5)
     date=driver.find_element_by_xpath("//span[@class='apm-filters__date']").click()
     prev_month=driver.find_element_by_xpath("//span[@class='prev']").click()
-    day=driver.find_elements_by_xpath("//span[@class='cell day']")[5].click()
+    day=driver.find_elements_by_xpath("//span[@class='cell day']")[5].click() 
     show=WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='apm-filters__btn_alt show_history']"))).click()
     time.sleep(5)
     bet_detail=driver.find_element_by_xpath("//div[@class='apm-panel-head__info']").text
     bet_status=re.findall(r'.*Bet slip status?\s+(Win|Loss|Unsettled).*',bet_detail)[0]
     print(bet_status)
     return 1
-print(bet_on_game('https://tvevyiehz.top/live/Basketball/2145513-PRO-League/312454136-Volga-Oka/',"yes",5000,"300244749","MyDream0","business"))
-
+#print(bet_on_game('https://ppbemizn.top/live/Basketball/112475-Lebanon-Championship/313137396-Sporting-Al-Riyadi-Beirut-Champville-SC/',"yes",5000,"300244749","MyDream0","business"))
+betting()
 
